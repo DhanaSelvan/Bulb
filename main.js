fdc2953f2576c65bae1f2d221dba3fbd8aa6b883
DhanaSelvan J U <93440351+DhanaSelvan@users.noreply.github.com>
2023-08-21 16:01:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function on(){
+    document.getElementById("Pic").src="https://toppng.com/public/uploads/thumbnail/light-bulb-on-off-png-115539403195dzrdrd6di.png";
+}
+
+function off(){
+    document.getElementById("Pic").src="https://toppng.com/public/uploads/thumbnail/light-bulb-on-off-png-11553940208gbhhx7rj3t.png";
+    console.log("Off button clicked");
+}
